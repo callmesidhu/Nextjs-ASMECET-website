8e6c0d05d1be513fd258dba2349390720eceb74c
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-09 19:09:30 +0530

    contact updated

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Contact.css';
+import Link from 'next/link';
 
 function Contact() {
   return (
@@ -9,13 +10,13 @@ function Contact() {
       
       <div className='flex flex-col lg:flex-row w-full justify-between my-8'>
         <address className='not-italic'>
-          <h3 className='inter text-gtext text-3xl m-3 mx-12'>asmecet@cet.ac.in</h3>
-          <h3 className='inter text-gtext text-3xl m-3 mx-12'>+91 987654321</h3>
+          <h3 className='inter text-gtext text-3xl m-3 mx-12'>asmecet@gmail.com</h3>
+          <h3 className='inter text-gtext text-3xl m-3 mx-12'>+91 7306732236</h3>
         </address>
         
         <div>
-          <h3 className='inter text-3xl m-3 mx-12'>@amseinsta</h3>
-          <h3 className='inter text-3xl m-3 mx-12'>@asmeyoutube</h3>
+        <Link href='https://www.instagram.com/asme_cet?igsh=MXB5bmczZzJnZXZkdA%3D%3D'><h3 className='inter text-3xl m-3 mx-12'>@instagram</h3></Link>
+        <Link href='https://www.linkedin.com/company/asme-cet/'><h3 className='inter text-3xl m-3 mx-12'>@linkedin</h3></Link> 
         </div>
       </div>
       
