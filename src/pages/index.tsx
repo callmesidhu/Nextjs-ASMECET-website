7c900d3fe13d406baa32794a178382626f90c172
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-31 04:30:08 +0530

    loader added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,29 @@
 // @/src/pages/index.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '@/Components/Navbar';
 import About from '@/Components/About';
 import Banner from '@/Components/Banner';
-import UpcomingEvents from '@/Components/Upcoming';
 import Contact from '@/Components/Contact';
-import {Intro} from '@/Components/Intro'
+import { Intro } from '@/Components/Intro';
 import Asme from '@/Components/Asme';
-
+import Loader from '@/pages/Loader';
 
 function HomePage() {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return loading ? <Loader /> : (
     <>
       <Navbar />
-      <Intro/>
+      <Intro />
       <Banner />
       <Asme />
       <About />
-      <Contact/>
+      <Contact />
     </>
   );
 }
